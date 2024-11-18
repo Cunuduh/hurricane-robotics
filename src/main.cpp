41747650bcd95ecb33ec0f6abfb92617b7ce062f
Cunuduh <nickleodean.email3@gmail.com>
2024-11-17 20:53:57 -0500

    reverse intake motor

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ void opcontrol()
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	pros::MotorGroup left_motors({-1, -3, 5}, pros::v5::MotorGears::blue);
 	pros::MotorGroup right_motors({2, 4, -6}, pros::v5::MotorGears::blue);
-	pros::Motor intake(21, pros::v5::MotorGears::green);
+	pros::Motor intake(-21, pros::v5::MotorGears::green);
 	pros::ADIDigitalOut solenoid('A');
 	static int intake_power = 0;
 	static bool intake_running = false;
