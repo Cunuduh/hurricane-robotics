2eb223b800116f7e68a1941ccdfae32ff47f1f44
cunuduh <nickleodean.email3@gmail.com>
2024-11-27 15:59:51 -0500

    Autonomous

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,145 @@
 #include "main.h"
-
-void on_center_button()
+pros::Optical colour_sensor(12);
+pros::Rotation lb_rotation(8);
+pros::MotorGroup intake({-21, -7}, pros::v5::MotorGears::green);
+std::atomic<int> intake_power{0};
+std::atomic<bool> intake_running{false};
+std::atomic<bool> colour_rejection_active{false};
+std::atomic<int> last_rejection_time{0};
+pros::Motor lb(13, pros::v5::MotorGears::green);
+pros::ADIDigitalOut solenoid('A');
+std::shared_ptr<okapi::ChassisController> chassis =
+	okapi::ChassisControllerBuilder()
+		.withMotors({-1, -3, 5}, {2, 4, -6})
+		.withDimensions(okapi::AbstractMotor::gearset::blue, {{3.25_in, 15_in}, okapi::imev5BlueTPR})
+		.build();
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+pros::MotorGroup left_motors({-1, -3, 5}, pros::v5::MotorGears::blue);
+pros::MotorGroup right_motors({2, 4, -6}, pros::v5::MotorGears::blue);
+enum class colour
 {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed)
+	RED,
+	BLUE,
+	NONE
+};
+colour team_colour = colour::BLUE;
+colour detect_colour()
+{
+	double hue = colour_sensor.get_hue();
+	if (hue >= 0 && hue <= 60)
 	{
-		pros::lcd::set_text(2, "I was pressed!");
+		master.print(0, 0, "Red ");
+		return colour::RED;
+	}
+	else if (hue >= 90 && hue <= 270)
+	{
+		master.print(0, 0, "Blue");
+		return colour::BLUE;
 	}
 	else
 	{
-		pros::lcd::clear_line(2);
+		master.print(0, 0, "None");
+		return colour::NONE;
 	}
 }
-
 void initialize()
 {
 	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::lcd::register_btn1_cb(on_center_button);
+	std::string team = (team_colour == colour::RED) ? "Red" : "Blue";
+	master.print(1, 0, ("Team: " + team).c_str());
+	intake_power = 0;
+	intake_running = false;
+	colour_rejection_active = false;
+	pros::Task colour_rejection_task{[&]
+	{
+		return;
+		while (true)
+		{
+			if (intake_power != 0)
+			{
+				colour detected = detect_colour();
+				if (detected != colour::NONE && detected != team_colour)
+				{
+					colour_rejection_active = true;
+					last_rejection_time = pros::millis();
+				}
+				if (colour_rejection_active && (pros::millis() - last_rejection_time > 800))
+				{
+					colour_rejection_active = false;
+					intake.move(intake_power);
+				}
+				else if (colour_rejection_active && (pros::millis() - last_rejection_time > 5))
+				{
+					intake.move(0);
+				}
+			}
+			pros::delay(30);
+		}
+	}};
 }
 void disabled() {}
 void competition_initialize() {}
+void activate_intake(int duration_ms = 0, int power = 127)
+{
+	intake.move(power);
+	if (duration_ms > 0)
+	{
+		pros::delay(duration_ms);
+		intake.move(0);
+	}
+}
+void activate_lb(int duration_ms = 0)
+{
+	lb.move(127);
+	pros::delay(duration_ms);
+	lb.move(-127);
+	pros::delay(duration_ms);
+	lb.move(0);
+}
 void autonomous()
 {
-	pros::Motor intake(21, pros::v5::MotorGears::green);
-	pros::ADIDigitalOut solenoid('A');
-	std::shared_ptr<okapi::ChassisController> chassis =
-		okapi::ChassisControllerBuilder()
-			.withMotors({-1, -3, 5}, {2, 4, -6})
-			.withDimensions(okapi::AbstractMotor::gearset::blue, {{3.25_in, 15_in}, okapi::imev5BlueTPR})
-			.build();
-
-	chassis->moveDistance(-0.5_ft);
+	chassis->setMaxVelocity(250);
+	solenoid.set_value(false);
+	chassis->moveDistance(-1.25_ft);
 	chassis->turnAngle(45_deg);
-	pros::delay(100);
-	chassis->moveDistance(-3.1_ft); // sqrt (2^2 + 2^2) = 2.83
+	chassis->setMaxVelocity(225);
+	chassis->moveDistance(-4_ft);
 	solenoid.set_value(true);
-	chassis->turnAngle(45_deg);
-	intake.move(127);
-	pros::delay(100);
+	chassis->moveDistance(-0.5_ft);
+	chassis->setMaxVelocity(250);
+	chassis->turnAngle(75_deg);
+	activate_intake();
+	chassis->moveDistance(3.5_ft);
+	pros::delay(1000);
+	activate_intake(0, 0);
+	chassis->moveDistance(-1.5_ft);
+	chassis->turnAngle(125_deg); // from 120
+	chassis->setMaxVelocity(75);
+	activate_intake();
+	chassis->moveDistance(1.5_ft);
+	pros::delay(2000);
+	activate_intake(0);
+}
+void autonomous_alt() {
+	chassis->moveDistance(-1_ft);
+	chassis->turnAngle(-45_deg);
+	chassis->moveDistance(-1.5_ft);
+	activate_intake(1000);
+	activate_lb(500);
+	chassis->turnAngle(67.5_deg);
+	chassis->moveDistance(-4.5_ft);
+	chassis->turnAngle(-112.5_deg);
+	pros::delay(500);
+	solenoid.set_value(true);
+	chassis->moveDistance(2_ft);
+	activate_intake(2000);
+	chassis->turnAngle(90_deg);
+	chassis->moveDistance(2_ft);
+	activate_intake(2000);
+	chassis->turnAngle(-45_deg);
 	chassis->moveDistance(3_ft);
-	intake.move(0);
+	solenoid.set_value(false);
+	chassis->moveDistance(8.5_ft);
 }
 int quad_curve(int input)
 {
@@ -53,24 +150,38 @@ int quad_curve(int input)
 }
 void opcontrol()
 {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_motors({-1, -3, 5}, pros::v5::MotorGears::blue);
-	pros::MotorGroup right_motors({2, 4, -6}, pros::v5::MotorGears::blue);
-	pros::Motor intake(21, pros::v5::MotorGears::green);
-	pros::ADIDigitalOut solenoid('A');
-	static int intake_power = 0;
-	static bool intake_running = false;
+	static bool override_active = false;
 	static bool solenoid_state = false;
-
 	while (true)
 	{
 		int left_input = quad_curve(master.get_analog(ANALOG_LEFT_Y));
 		int right_input = quad_curve(master.get_analog(ANALOG_RIGHT_Y));
+		if (master.get_digital_new_press(DIGITAL_X))
+		{
+			override_active = !override_active;
+			
+		}
 		if (master.get_digital_new_press(DIGITAL_A))
 		{
 			autonomous();
 		}
-		if (master.get_digital_new_press(DIGITAL_L2) && !solenoid_state)
+		if (master.get_digital_new_press(DIGITAL_B))
+		{
+			autonomous_alt();
+		}
+		if (master.get_digital(DIGITAL_RIGHT))
+		{
+			lb.move(127);
+		}
+		else if (master.get_digital(DIGITAL_LEFT))
+		{
+			lb.move(-127);
+		}
+		else
+		{
+			lb.move(0);
+		}
+		if (master.get_digital_new_press(DIGITAL_L2))
 		{
 			intake_running = true;
 			intake_power = 127;
@@ -88,6 +199,14 @@ void opcontrol()
 		{
 			intake_power = intake_running ? 127 : 0;
 		}
+		if (colour_rejection_active)
+		{
+			master.print(2, 0, "True ");
+		}
+		else
+		{
+			master.print(2, 0, "False");
+		}
 		if (master.get_digital_new_press(DIGITAL_R1))
 		{
 			solenoid_state = !solenoid_state;
@@ -96,7 +215,10 @@ void opcontrol()
 
 		left_motors.move(left_input);
 		right_motors.move(right_input);
-		intake.move(intake_power);
+		if (!colour_rejection_active || override_active)
+		{
+			intake.move(intake_power);
+		}
 
 		std::vector<double> left_temp = left_motors.get_temperature_all();
 		std::vector<double> right_temps = right_motors.get_temperature_all();
