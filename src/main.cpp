9bf2ea57fa3cbefd411e2b6831750fcc02237953
Cunuduh <nickleodean.email3@gmail.com>
2025-02-11 10:55:21 -0500

    autonomous part 1

--- src/main.cpp
+++ src/main.cpp
@@ -18,11 +18,7 @@ int32_t intake_power = 0;
 bool intake_running = false;
 bool solenoid_on = false;
 bool doinker_on = false;
-bool climb_on = false;
 
-double lb_presets[] = {5.0, 17.0, 110.0};
-size_t lb_preset_index = 0;
-bool moving_lb = false;
 double get_lb_angle()
 {
 	return lb_rotation.get_position() / 100.0;
@@ -94,25 +90,25 @@ void autonomous()
 	activate_intake(2000); // Deposit preload ring into alliance wall stake
 	chassis->moveDistance(1.25_ft);
 	chassis->turnAngle(90_deg);
-	chassis->moveDistance(-3_ft); // pick up first mobile goal
+	chassis->moveDistance(-2.25_ft); // pick up first mobile goal
 	activate_sol();
-	chassis->turnAngle(180_deg);
-	activate_intake();
-	chassis->moveDistance(4_ft);
-	chassis->moveDistance(-2_ft);
-	chassis->turnAngle(-90_deg);
-	chassis->moveDistance(2_ft); // nab three rings top left corner
+	chassis->moveDistance(1_ft);
 	chassis->moveDistance(-1_ft);
-	chassis->turnAngle(180_deg);
-	chassis->moveDistance(3.25_ft);
-	chassis->turnAngle(22.5_deg);
-	chassis->moveDistance(1.8_ft);
-	chassis->turnAngle(167_deg);
-	chassis->moveDistance(3.75_ft);
-	chassis->turnAngle(-105_deg);
-	chassis->moveDistance(-5.7_ft);
+	chassis->turnAngle(-90_deg);
+	activate_intake();
+	chassis->moveDistance(2.25_ft); // pick up first ring
+	chassis->turnAngle(-56.3_deg); // arctan(3/2)
+	chassis->moveDistance(3.75_ft); // pick up second ring
+	chassis->turnAngle(-146.2_deg);
+	chassis->moveDistance(2.25_ft); // pick up third ring
+	chassis->turnAngle(33.7_deg);
+	chassis->moveDistance(3.5_ft); // pick up fourth and fifth rings
+	chassis->moveDistance(-0.5_ft);
+	chassis->turnAngle(135_deg);
+	chassis->moveDistance(1.5_ft); // pick up sixth ring
+	chassis->turnAngle(90_deg);
+	chassis->moveDistance(2_ft); // back up into corner
 	activate_sol();
-	
 }
 int cube_curve(int input, int max_rpm)
 {
@@ -127,17 +123,23 @@ void opcontrol()
 		bool l2_press = master.get_digital_new_press(DIGITAL_L2);
 		bool r2_press = master.get_digital_new_press(DIGITAL_R2);
 		bool r1_press = master.get_digital_new_press(DIGITAL_R1);
+		bool l1_held = master.get_digital_new_press(DIGITAL_L1);
 		bool a_press = master.get_digital_new_press(DIGITAL_A);
+		bool b_press = master.get_digital_new_press(DIGITAL_B);
 		bool x_press = master.get_digital_new_press(DIGITAL_X);
+		bool y_press = master.get_digital_new_press(DIGITAL_Y);
+		bool right_held = master.get_digital(DIGITAL_RIGHT);
+		bool left_held = master.get_digital(DIGITAL_LEFT);
 		int32_t analog_left_y = master.get_analog(ANALOG_LEFT_Y);
 		int32_t analog_right_x = master.get_analog(ANALOG_RIGHT_X);
+
 
 		int32_t power = cube_curve(analog_left_y, 600);
 		int32_t turn = cube_curve(analog_right_x, 600);
 		int32_t left_input = power + turn;
 		int32_t right_input = power - turn;
 
-		if (master.get_digital_new_press(DIGITAL_Y) && !pros::competition::is_connected())
+		if (y_press && !pros::competition::is_connected())
 			autonomous();
 
 		if (l2_press)
@@ -151,7 +153,7 @@ void opcontrol()
 
 		if (intake_running)
 		{
-			if (master.get_digital(DIGITAL_L1))
+			if (l1_held)
 			{
 				intake_power = -200;
 			}
@@ -176,66 +178,18 @@ void opcontrol()
 			doinker.set_value(doinker_on);
 		}
 
-		if (master.get_digital_new_press(DIGITAL_LEFT))
-		{
-			if (lb_preset_index > 0)
-			{
-				lb_preset_index--;
-				moving_lb = true;
-			}
-		}
-		if (master.get_digital_new_press(DIGITAL_RIGHT)) 
-		{
-			if (lb_preset_index < 2)
-			{
-				lb_preset_index++;
-				moving_lb = true;
-			}
-		}
-
-		if (master.get_digital(DIGITAL_UP))
+		if (right_held)
 		{
 			lb.move_velocity(100);
 		}
-		else if (master.get_digital(DIGITAL_DOWN))
+		else if (left_held)
 		{
 			lb.move_velocity(-100);
-		}
-		else if (moving_lb)
-		{
-			double current_angle = get_lb_angle();
-			double target_angle = lb_presets[lb_preset_index];
-			double error = target_angle - current_angle;
-			
-			if (std::fabs(error) > 5)
-			{
-				int vel = static_cast<int>(error * 2);
-				if (vel > 100)
-				{
-					vel = 100;
-				}
-				if (vel < -100)
-				{
-					vel = -100;
-				}
-				lb.move_velocity(vel);
-			}
-			else
-			{
-				lb.move_velocity(0);
-				moving_lb = false;
-			}
-		}
-		else
-		{
-			lb.move_velocity(0);
 		}
 
 		intake.move_velocity(intake_power);
 		left_motors.move_velocity(left_input);
 		right_motors.move_velocity(right_input);
-		pros::lcd::print(1, "LB: %.2f", get_lb_angle());
-		pros::lcd::print(2, "LB Preset: %d ", lb_preset_index);
 		pros::delay(3);
 	}
 }
